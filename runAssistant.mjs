0c1e78ae34cc6837fb900b15255714f31a16d2d0
kostyasabada <ksabada@mycubes.nl>
2023-12-12 16:38:38 +0200

    import modules

---
+++ runAssistant.mjs
@@ -0,0 +1,31 @@
+
+import dotenv from 'dotenv'
+dotenv.config();
+import { OpenAI } from 'openai';
+import * as readline from 'node:readline/promises';
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+// Create a OpenAI connection
+const secretKey = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({
+  apiKey: secretKey,
+});
+
+async function askQuestion(question) {
+  const answer = await rl.question(question);
+  console.log('answer:::', answer);
+  return answer;
+}
+
+async function main() {
+  try {
+   
+    // close the readline
+    rl.close();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Call the main function
+main();
